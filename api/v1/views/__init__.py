9918cc4cd5fa43115fe26d4ce53570c2abf81214
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-10-30 08:00:38 -0700

    Api status

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-
-"""
-creates the app_views Blueprint with a URL
-prefix of /api/v1. It also imports the views
-you want to include in the blueprint.
-"""
-
+""" Blueprint for API """
 from flask import Blueprint
-from api.v1.views.index import *
-
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
