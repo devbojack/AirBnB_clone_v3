6b97faf547ecc07a03036f642d129eb4eebc4c5b
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-10-30 08:11:30 -0700

    Add stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,8 +7,21 @@ which returns a JSON response with the status "OK" when accessed.
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    stats_dict = {}
+    classes = ["User", "State", "City", "Amenity", "Place", "Review"]
+
+    for cls in classes:
+        count = storage.count(cls)
+        stats_dict[cls] = count
+
+    return jsonify(stats_dict)
